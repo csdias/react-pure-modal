98f15e1f796d2a795e8b4186e0f696ce03936a93
Carlos Soares Dias <csdias@gmail.com>
2019-12-10 01:00:02 -0300

    commitao

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import Modal from './Modal';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isModalOpen: false
+  };
+toggleState = e => {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  };
+render() {
+    return (
+      <div>
+        <button onClick={this.toggleState}>Open Modal</button>
+        <div>Modal is: {this.state.isModalOpen ? "Open" : "Closed"}</div>
+        {this.state.isModalOpen && (
+          <Modal
+            id="modal"
+            isOpen={this.state.isModalOpen}
+            onClose={this.toggleState}
+            class="my-class"
+           >
+            <div className="box-body">I am the content of the modal</div>
+          </Modal>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+//export default App;
